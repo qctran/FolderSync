c610d17e2fc97127c91a8884bab47ff809e0a620
qctran <conantran@outlook.com>
2022-03-08 17:25:57 -0800

    Initial checking
    Some UI works still need to be completed

---
+++ FolderSync/Models/FileModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSync.Models
+{
+    public class FileModel
+    {
+        public string File { get; set; }
+        public int Code { get; set; }   // 0: Same, 1: New & 2: Delete
+        public string ModifiedDate { get; set; }
+    }
+}
---
+++ FolderSync/ViewModels/OperationViewModel.cs
@@ -0,0 +1,104 @@
+﻿using FolderSync.Infrastructure;
+using FolderSync.Models;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FolderSync.ViewModels
+{
+    public class OperationViewModel : Infrastructure.ModelNotification
+    {
+        private string? _sourcePath;
+        private string? _targetPath;
+        private IList<FileModel> _fileList = new List<FileModel>();
+
+        private ICommand _analyzeCmd;
+        public ICommand AnalyzeCmd
+        {
+            get
+            {
+                if (_analyzeCmd == null) _analyzeCmd = new RelayCommand(p => OnAnalyze());
+                return _analyzeCmd;
+            }
+        }
+
+        private void OnAnalyze()
+        {
+            Console.WriteLine("OnAnalyze");
+        }
+
+        private ICommand _execCmd;
+        public ICommand ExecCmd
+        {
+            get
+            {
+                if (_execCmd == null) _execCmd = new RelayCommand(p => OnExec());
+                return _execCmd;
+            }
+        }
+
+        private void OnExec()
+        {
+            Console.WriteLine("OnExec");
+        }
+
+        private ICommand _browseSourceCmd;
+        public ICommand BrowseSourceCmd
+        {
+            get
+            {
+                if (_browseSourceCmd == null ) _browseSourceCmd = new RelayCommand(p => OnBrowseSource());
+                return _browseSourceCmd;
+            }
+        }
+
+        private void OnBrowseSource()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                var filename = dlg.FileName;
+                SourcePath = Path.GetDirectoryName(filename);
+                Console.WriteLine($"Dir: {SourcePath}");
+            }
+        }
+
+        private ICommand _browseTargetCmd;
+        public ICommand BrowseTargetCmd
+        {
+            get
+            {
+                if (_browseTargetCmd == null) _browseTargetCmd = new RelayCommand(p => OnBrowseTarget());
+                return _browseTargetCmd;
+            }
+        }
+
+        private void OnBrowseTarget()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                var filename = dlg.FileName;
+                TargetPath = Path.GetDirectoryName(filename);
+                Console.WriteLine($"Dir: {TargetPath}");
+            }
+        }
+
+        public string SourcePath { get { return _sourcePath; } set { _sourcePath = value; OnPropertyChanged("SourcePath"); } }
+        public string TargetPath { get { return _targetPath; } set { _targetPath = value; OnPropertyChanged("TargetPath"); } }
+        public IList<FileModel> FileList { get { return _fileList; } set { _fileList = value; OnPropertyChanged("FileList"); } }
+
+        
+       
+        public OperationViewModel()
+        {
+            _fileList.Clear();
+            FileList.Add(new FileModel{ File = @"C:\temp", Code = 1, ModifiedDate = DateTime.Now.ToShortDateString() });
+        }
+    }
+}
